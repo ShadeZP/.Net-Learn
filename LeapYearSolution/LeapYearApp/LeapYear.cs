56c3be4619d4c0b5c20381c0176dff3bd3b5e259
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-07 22:15:40 +0300

    Implement minimal logic for leap year 1996
    Updated IsLeap method to return true only for year 1996.
    Allows Returns_true_for_typical_leap_year test to pass (Green phase).

--- LeapYearSolution/LeapYearApp/LeapYear.cs
+++ LeapYearSolution/LeapYearApp/LeapYear.cs
@@ -4,6 +4,8 @@ public class LeapYear
 {
     public static bool IsLeap(int year)
     {
+        if (year == 1996)
+            return true;
         throw new System.NotImplementedException();
     }
 }
