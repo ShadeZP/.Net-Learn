3be48609f274a66022cc479a0353bbda577dea13
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-07 22:13:49 +0300

    Add failing test for typical leap year 1996
    Added Returns_true_for_typical_leap_year test in LeapYearTests.
    Checks that IsLeap(1996) returns true as expected for leap years.
    Implementation not yet provided, so test fails (Red phase).

--- LeapYearSolution/LeapYearApp.Tests/LeapYearTests.cs
+++ LeapYearSolution/LeapYearApp.Tests/LeapYearTests.cs
@@ -3,8 +3,8 @@
 public class LeapYearTests
 {
     [Fact]
-    public void Test1()
+    public void Returns_true_for_typical_leap_year()
     {
-
+        Assert.True(LeapYear.IsLeap(1996));
     }
 }
