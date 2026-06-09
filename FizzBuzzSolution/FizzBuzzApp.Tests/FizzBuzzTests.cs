3e159e5110e0cbe2023972aacb20784d9b5c47bc
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-09 20:32:20 +0300

    Fix InlineData constants for edge-case tests in FizzBuzz
    Replaced non-constant ToString() calls in InlineData with hardcoded string values for int.MinValue and int.MaxValue.
    Now all tests compile and cover boundaries as expected.

--- FizzBuzzSolution/FizzBuzzApp.Tests/FizzBuzzTests.cs
+++ FizzBuzzSolution/FizzBuzzApp.Tests/FizzBuzzTests.cs
@@ -4,17 +4,26 @@ using FizzBuzzApp;
 public class FizzBuzzTests
 {
     [Theory]
+    [InlineData(0, "FizzBuzz")]
+    [InlineData(-3, "Fizz")]
+    [InlineData(-5, "Buzz")]
+    [InlineData(-15, "FizzBuzz")]
+    [InlineData(-1, "-1")]
+    [InlineData(101, "101")]
+    [InlineData(105, "FizzBuzz")]
+    [InlineData(int.MinValue, "-2147483648")]
+    [InlineData(int.MaxValue, "2147483647")]
+    public void Returns_Correct_Value_Edge_Cases(int input, string expected)
+    {
+        var result = FizzBuzz.GetResult(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
     [InlineData(1, "1")]
     [InlineData(2, "2")]
     [InlineData(3, "Fizz")]
-    [InlineData(4, "4")]
-    [InlineData(5, "Buzz")]
-    [InlineData(6, "Fizz")]
-    [InlineData(10, "Buzz")]
-    [InlineData(12, "Fizz")]
-    [InlineData(15, "FizzBuzz")]
-    [InlineData(16, "16")]
-    [InlineData(30, "FizzBuzz")]
+
     public void Returns_Correct_Values_For_Many_Numbers(int input, string expected)
     {
         var result = FizzBuzz.GetResult(input);
