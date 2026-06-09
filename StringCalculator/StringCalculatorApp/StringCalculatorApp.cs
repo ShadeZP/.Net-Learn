919b7d4233e79b86d2b901675ed7c8acd3b46312
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-09 20:20:38 +0300

    Implement exception throwing for negative numbers in Add
    Add now throws ArgumentException with message "negatives not allowed: ..." containing all negative numbers found in input.
    Passes Throws_exception_when_negative_numbers_are_passed test and all previous tests (Green phase).

--- StringCalculator/StringCalculatorApp/StringCalculatorApp.cs
+++ StringCalculator/StringCalculatorApp/StringCalculatorApp.cs
@@ -11,16 +11,24 @@ public class StringCalculatorApp
         string numbersPart = numbers;
 
         if (
-               numbers.Length > 2 &&
-               !char.IsDigit(numbers[0]) && numbers[1] == '\n'
-           )
+            numbers.Length > 2 &&
+            !char.IsDigit(numbers[0]) && numbers[1] == '\n'
+        )
         {
             delimiters = [numbers[0], ',', '\n'];
             numbersPart = numbers.Substring(2);
         }
 
-        var numberStrings = numbersPart.Split(delimiters);
+        var numberStrings = numbersPart.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
 
-        return numberStrings.Sum(s => int.Parse(s));
+        var ints = numberStrings.Select(s => int.Parse(s)).ToList();
+
+        var negatives = ints.Where(n => n < 0).ToList();
+        if (negatives.Any())
+        {
+            throw new ArgumentException("negatives not allowed: " + string.Join(',', negatives));
+        }
+
+        return ints.Sum();
     }
 }
