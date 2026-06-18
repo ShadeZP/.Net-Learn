09244e3954c668dc8f55ddc71af91870f9c20396
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-18 14:11:09 +0300

    feat: add trigger

---
+++ SQL-Task/CompanyDB/Triggers/AfterInsertEmployee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER AfterInsert_Employee
+ON [dbo].[Employee]
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO [dbo].[Company] ([Name], [AddressId])
+    SELECT i.CompanyName, i.AddressId
+    FROM inserted i;
+END
