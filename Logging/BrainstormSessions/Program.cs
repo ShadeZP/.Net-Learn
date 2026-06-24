bdd8435ad624744b2f2c3b65a201563b0fa8c5b0
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2026-06-25 00:28:35 +0300

    feat: add serilog to project and basic config for it

--- Logging/BrainstormSessions/Program.cs
+++ Logging/BrainstormSessions/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace BrainstormSessions
 {
@@ -7,11 +9,28 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
